5cff006a84564ea0863a20db7eaffde28bdb834b
Eduxrduu <duduarara2307@gmail.com>
2021-01-10 03:52:49 -0300

    castle

---
+++ PlayerMoviment.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float Jumphigh = 3f;
+    public Transform GroundCheck;
+    public float groundistance = 0.4f;
+    public LayerMask groundmask;
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundistance, groundmask);
+
+        if (isGrounded && velocity.y < -2fs)
+        {
+            velocity.y = 0f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(Jumphigh * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
